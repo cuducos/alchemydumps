dc9cea90b8f285ae740cdb801552af4ea47970dd
Eduardo Cuducos <cuducos@gmail.com>
2014-11-15 21:06:35 +0000

    distutils fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 
-# for vagrant (remove is running elsewhere)
-import os
-del os.link
-
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='flask-alchemydumps',
+setup(name='Flask-AlchemyDumps',
       version='0.0.1',
       description='SQLAlchemy backup/dump tool for Flask',
       long_description=readme(),
@@ -19,7 +15,6 @@ setup(name='flask-alchemydumps',
         'Programming Language :: Python :: 2.7',
         'Intended Audience :: Developers',
         'Topic :: Database',
-        'Topic :: Internet :: WWW/HTTP :: Site Management',
         'Topic :: System :: Archiving :: Backup',
         'Topic :: Utilities'
       ],
@@ -38,5 +33,5 @@ setup(name='flask-alchemydumps',
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
-      include_package_data=False,
+      include_package_data=True,
       zip_safe=False)
