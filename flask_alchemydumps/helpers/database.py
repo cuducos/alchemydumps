fd92f7ba7a331c0fee1140f4666d5634c74ffd71
Eduardo Cuducos <cuducos@gmail.com>
2015-06-23 10:39:25 +0100

    Add support for abstract models/classes

--- flask_alchemydumps/helpers/database.py
+++ flask_alchemydumps/helpers/database.py
@@ -8,6 +8,7 @@ class AlchemyDumpsDatabase(object):
 
     def __init__(self):
         self.do_not_backup = list()
+        self.models = list()
 
     @staticmethod
     def db():
@@ -16,11 +17,16 @@ class AlchemyDumpsDatabase(object):
     def get_mapped_classes(self):
         """Gets a list of SQLALchemy mapped classes"""
         db = self.db()
-        models = list()
-        for model_name in db.Model.__subclasses__():
-            if model_name not in self.do_not_backup:
-                models.append(model_name)
-        return models
+        self.add_subclasses(db.Model)
+        return self.models
+
+    def add_subclasses(self, model):
+        """Feed self.models filtering `do_not_backup` and abstract models"""
+        if model.__subclasses__():
+            for submodel in model.__subclasses__():
+                self.add_subclasses(submodel)
+        else:
+            self.models.append(model)
 
     def get_data(self):
         """Go through every mapped class and dumps the data"""
