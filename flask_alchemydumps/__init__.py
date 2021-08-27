5ac6ca87a5275b539959bdb9b595b86ab85c92d2
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-08-27 17:15:14 -0400

    Fixes subcommand
    Fixes #24
    
    Thanks @woopla

--- flask_alchemydumps/__init__.py
+++ flask_alchemydumps/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from flask_alchemydumps.cli import alchemydumps
 
 
-class _AlchemyDumpsConfig:
+class AlchemyDumpsConfig:
     def __init__(self, db=None, basedir=""):
         self.db = db
         self.basedir = Path(basedir).absolute()
@@ -13,11 +13,13 @@ class AlchemyDumps:
     def __init__(self, app=None, db=None, basedir=""):
         if app is not None and db is not None:
             self.init_app(app, db, basedir)
-        if app is not None:
-            app.cli.add_command(alchemydumps)
 
     @staticmethod
     def init_app(app, db, basedir=""):
         if not hasattr(app, "extensions"):
             app.extensions = {}
-        app.extensions["alchemydumps"] = _AlchemyDumpsConfig(db, basedir)
+
+        app.extensions["alchemydumps"] = AlchemyDumpsConfig(db, basedir)
+
+        if app is not None:
+            app.cli.add_command(alchemydumps)
